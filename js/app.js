10927e335b19a9c19acd7672189dbdfbb98b96f3
Fanyam Terkuma <lawrencefanyam@gmail.com>
2025-05-28 15:14:21 +0100

    Generate a 16x16 grid with flexbox

--- js/app.js
+++ js/app.js
@@ -1,7 +1,24 @@
 const container = document.querySelector('.container');
 //create grid items
-for (let i = 0; i < 16 * 16; i++) {
-    const gridItem = document.createElement('div');
-    gridItem.classList.add('grid-item');
-    container?.appendChild(gridItem);
-}
+
+container.addEventListener('mouseover', e => {
+    if (e.target.classList.contains('grid-item')) {
+        e.target.classList.add('grid-item-touched');
+    }
+});
+
+const button = document.querySelector('.btn');
+
+button.addEventListener('click', () => {
+    let grdDimension = 16;
+    let customGridDimension = prompt('Enter custom dimension for new layout');
+    if (customGridDimension != null) {
+        gridDimension = customGridDimension;
+        for (let i = 0; i < gridDimension * gridDimension; i++) {
+            const gridItem = document.createElement('div');
+            gridItem.classList.add('grid-item');
+            gridItem.style.flexBasis = `calc((100%/${gridDimension}))`;
+            container?.appendChild(gridItem);
+        }
+    }
+});
